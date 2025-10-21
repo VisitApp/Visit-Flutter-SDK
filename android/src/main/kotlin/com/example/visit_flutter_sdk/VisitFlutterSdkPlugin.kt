30660bbca9fc6364457b5d9dffb60538d411b7c8
chinmay <chinmaydash11@gmail.com>
2025-10-21 19:12:54 +0530

    tested method channel for battery status.

--- android/src/main/kotlin/com/example/visit_flutter_sdk/VisitFlutterSdkPlugin.kt
+++ android/src/main/kotlin/com/example/visit_flutter_sdk/VisitFlutterSdkPlugin.kt
@@ -1,33 +1,79 @@
 package com.example.visit_flutter_sdk
 
+import android.app.Activity
+import android.content.Context
+import android.os.BatteryManager
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** VisitFlutterSdkPlugin */
-class VisitFlutterSdkPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class VisitFlutterSdkPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private lateinit var channel: MethodChannel
+    private lateinit var appContext: Context
+    private var activity: Activity? = null
 
-  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "visit_flutter_sdk")
-    channel.setMethodCallHandler(this)
-  }
 
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        appContext = flutterPluginBinding.applicationContext
+
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "visit_flutter_sdk")
+        channel.setMethodCallHandler(this)
     }
-  }
 
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    // ActivityAware hooks
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity
+        // If you need permission/result callbacks, add listeners to binding here
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        activity = null
+    }
+
+    override fun onDetachedFromActivity() {
+        activity = null
+    }
+
+
+    override fun onMethodCall(call: MethodCall, result: Result) {
+        if (call.method == "getPlatformVersion") {
+            result.success("Android ${android.os.Build.VERSION.RELEASE}")
+        } else if (call.method == "getBatteryLevel") {
+            val batteryLevel = getBatteryLevel()
+
+            if (batteryLevel != -1) {
+                result.success(batteryLevel)
+            } else {
+                result.error("UNAVAILABLE", "Battery level not available.", null)
+            }
+        } else {
+            result.notImplemented()
+        }
+    }
+
+
+    private fun getBatteryLevel(): Int {
+        val batteryLevel: Int
+        val batteryManager = appContext.getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+        batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+
+        return batteryLevel
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 }
--- lib/visit_android_webview/visit_android_webview.dart
+++ lib/visit_android_webview/visit_android_webview.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:path_provider/path_provider.dart';
@@ -27,6 +28,9 @@ class VisitAndroidWebView extends StatefulWidget {
 }
 
 class _VisitAndroidWebViewState extends State<VisitAndroidWebView> {
+  static const platform = MethodChannel('visit_flutter_sdk');
+  String _batteryLevel = 'Unknown battery level.';
+
   late InAppWebViewController _webViewController;
   String TAG = "mytag";
   bool _isLoading = false;
@@ -86,8 +90,9 @@ class _VisitAndroidWebViewState extends State<VisitAndroidWebView> {
                       try {
                         String jsonString = args[0];
 
-                        Map<String, dynamic> callbackResponse =
-                        jsonDecode(jsonString);
+                        Map<String, dynamic> callbackResponse = jsonDecode(
+                          jsonString,
+                        );
 
                         if (widget.isLoggingEnabled) {
                           print("$TAG: callbackResponse: $callbackResponse");
@@ -117,6 +122,8 @@ class _VisitAndroidWebViewState extends State<VisitAndroidWebView> {
                         } else if (methodName == "OPEN_DAILER") {
                           int? phone = callbackResponse['number'];
                           _makePhoneCall(phone!);
+                        } else if (methodName == "BATTERY_STATUS") {
+                          _getBatteryLevel();
                         }
                       } catch (e) {
                         print("$TAG: args: $e");
@@ -158,17 +165,34 @@ class _VisitAndroidWebViewState extends State<VisitAndroidWebView> {
             ),
           ),
           if (_isLoading)
-            const Center(
-                child: Align(
-                  alignment: Alignment(0.0, 0.7),
-                  // Align at 0.8 part of the screen height
-                  child: CircularProgressIndicator(
-                    color: Color(0xFFEC6625),
-                  ),
-                )),
+            const Align(
+              alignment: Alignment(0.0, 0.7),
+              // Align at 0.8 part of the screen height
+              child: CircularProgressIndicator(color: Color(0xFFEC6625)),
+            ),
+
+          Align(
+            alignment: Alignment(0.0, 0.7),
+            // Align at 0.8 part of the screen height
+            child: Text("Battery Level: $_batteryLevel"),
+          ),
         ],
       ),
     );
+  }
+
+  Future<void> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final result = await platform.invokeMethod<int>('getBatteryLevel');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
   }
 
   Map<String, String> _buildHeaders(String? authToken) {
@@ -366,23 +390,29 @@ class _VisitAndroidWebViewState extends State<VisitAndroidWebView> {
 
   _showEnableGPSDialog() async {
     return showPermissionDialog(
-        context, 'Please go to settings and turn on GPS',
-        onPositiveButtonPress: () {
-          Navigator.of(context).pop();
-          Geolocator.openLocationSettings();
-        }, onNegativeButtonPress: () {
-      Navigator.of(context).pop();
-    });
+      context,
+      'Please go to settings and turn on GPS',
+      onPositiveButtonPress: () {
+        Navigator.of(context).pop();
+        Geolocator.openLocationSettings();
+      },
+      onNegativeButtonPress: () {
+        Navigator.of(context).pop();
+      },
+    );
   }
 
   void _showAndroidPermissionDialog() {
     showPermissionDialog(
-        context, 'Please go to setting and turn on the permission',
-        onPositiveButtonPress: () {
-          Navigator.of(context).pop();
-          openAppSettings();
-        }, onNegativeButtonPress: () {
-      Navigator.pop(context);
-    });
+      context,
+      'Please go to setting and turn on the permission',
+      onPositiveButtonPress: () {
+        Navigator.of(context).pop();
+        openAppSettings();
+      },
+      onNegativeButtonPress: () {
+        Navigator.pop(context);
+      },
+    );
   }
 }
